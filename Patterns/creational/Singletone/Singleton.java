fec409cc144f67f6bbb7e1f0b69b067a491283ba
Kirill Lypkan <lypkankirill@gmail.com>
2020-09-08 17:25:57 +0300

    singleton pattern

--- Patterns/creational/Singletone/Singleton.java
+++ Patterns/creational/Singletone/Singleton.java
@@ -9,7 +9,9 @@ public final class Singleton {
     public String value;
 
     private Singleton(String value) {
-        // Этот код эмулирует медленную инициализацию.
+        // EN: The following code emulates slow initialization.
+        //
+        // RU: Этот код эмулирует медленную инициализацию.
         try {
             Thread.sleep(1000);
         } catch (InterruptedException ex) {
@@ -19,7 +21,7 @@ public final class Singleton {
     }
 
     public static Singleton getInstance(String value) {
-        if (instance == null)   {
+        if (instance == null) {
             instance = new Singleton(value);
         }
         return instance;
